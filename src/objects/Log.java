351189dc393269c9cd76da2419440e7f994d2e59
Rodrigo Carraça <rmsca4@iscte-iul.pt>
2025-11-30 23:09:21 +0000

    Update Log.java

--- src/objects/Log.java
+++ src/objects/Log.java
@@ -5,42 +5,55 @@ import pt.iscte.poo.utils.Vector2D;
 import pt.iscte.poo.game.Room;
 
 public class Log extends Movable implements Crushable {
-	public Log(Point2D p, Room r) {
-		super(p, r);
-	}
+    public Log(Point2D p, Room r) {
+        super(p, r);
+    }
 
-	@Override
-	public String getName() {
-		return "log";
-	}
+    @Override
+    public String getName() {
+        return "trunk";
+    }
 
-	@Override
-	public int getLayer() {
-		return 2;
-	}
+    @Override
+    public int getLayer() {
+        return 2;
+    }
 
-	@Override
-	public boolean isTransposable() {
-		return false;
-	}
+    @Override
+    public boolean isTransposable() {
+        return false;
+    }
 
-	@Override
-	public void move(Vector2D d) {
-		/* static */ }
+    @Override
+    public void move(Vector2D d) {
+        /* static */ 
+    }
 
-	@Override
-	public int mutation() {
-		return 0;
-	}
+    @Override
+    public int mutation() {
+        return 0;
+    }
 
-	@Override
-	public Weight getWeight() {
-		// TODO Auto-generated method stub
-		return Weight.LIGHT;
-	}
+    @Override
+    public Weight getWeight() {
+        return Weight.LIGHT;
+    }
 
-	@Override
-	public void onCrushedBy(Room room, GameObject crusher, Point2D pos) {
-		room.removeObject(this);
-	}
+    /**
+     * Log não cai por si — onFall deixa-o onde está.
+     * O comportamento de partir quando um HEAVY cai sobre ele
+     * é tratado por Crushable.onCrushedBy(), chamado por Room.applyGravity().
+     */
+    @Override
+    public void onFall(Room room, Point2D from, Point2D to) {
+        // não faz nada (log é estático)
+    }
+
+    /** Quando esmagado por um objeto pesado, parte-se (remove-se da sala). */
+    @Override
+    public void onCrushedBy(Room room, GameObject crusher, Point2D pos) {
+        if (room == null)
+            return;
+        room.removeObject(this);
+    }
 }
