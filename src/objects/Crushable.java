6cae61d23b34cbade3da65e38d450bf843cb93af
Rodrigo Carraça <rmsca4@iscte-iul.pt>
2025-11-30 23:07:55 +0000

    Update Crushable.java

--- src/objects/Crushable.java
+++ src/objects/Crushable.java
@@ -1,13 +1,8 @@
 package objects;
 
-import pt.iscte.poo.game.Room;
 import pt.iscte.poo.utils.Point2D;
+import pt.iscte.poo.game.Room;
 
-/**
- * Marca um objecto que pode ser esmagado por outro objecto.
- * onCrushedBy é invocado quando 'crusher' (o objecto que cai) esmaga este objecto
- * na posição 'pos' dentro de 'room'.
- */
 public interface Crushable {
     void onCrushedBy(Room room, GameObject crusher, Point2D pos);
 }
